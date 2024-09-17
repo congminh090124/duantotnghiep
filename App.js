7a89e150f1492698f4cc78c740772f7bc43180c5
vo ngoc chinh <vongocchinh001@gmail.com>
2024-09-17 16:21:06 +0700

    cac giao dien con lai

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import QuenMatKhauScreen from './screens/QuenMatKhauScreen'; // Import your QuenMatKhauScreen component
-import XacMinhOtpScreen from './screens/XacMinhOtpScreen'; // Import your QuenMatKhauScreen component
-import DangKiDulichScreen from './screens/DangKiDuLichScreen'; // Import your QuenMatKhauScreen component
-import DangBaiScreen from './screens/DangBaiScreen'; // Import your QuenMatKhauScreen component
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Image } from 'react-native'; // Thêm import Image
+// Import các màn hình
+import QuenMatKhauScreen from './screens/QuenMatKhauScreen';
+import XacMinhOtpScreen from './screens/XacMinhOtpScreen';
+import DangKiDulichScreen from './screens/DangKiDuLichScreen';
+import DangBaiScreen from './screens/DangBaiScreen';
 import NhanTinScreen from './screens/NhanTinScreen';
 import DangKy from './screens/Dangky';
 import DangNhap from './screens/DangNhap';
@@ -14,85 +17,69 @@ import ThongTinCaNhan from './screens/ThongTinCaNhan';
 import TimKiem from './screens/TimKiem';
 import Blog from './screens/Blog';
 import ThongBao from './screens/ThongBao';
+import DangKiTinhNguyenVienScreen from './screens/DangKiTinhNguyenVienScreen';
 
+// Tạo Stack Navigator
 const Stack = createStackNavigator();
 
+// Tạo Bottom Tab Navigator
+const Tab = createBottomTabNavigator();
+
+// Tạo các màn hình trong Bottom Tab
+const BottomTabs = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ size }) => {
+          let iconPath;
+
+          if (route.name === 'Home') {
+            iconPath = require('./assets/home.png');
+          } else if (route.name === 'Search') {
+            iconPath = require('./assets/search.png');
+          } else if (route.name === 'Notifications') {
+            iconPath = require('./assets/notifications.png');
+          } else if (route.name === 'Messages') {
+            iconPath = require('./assets/messages.png');
+          }
+
+          return <Image source={iconPath} style={{ width: size, height: size }} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Home" component={DangKiDulichScreen} options={{ headerShown: false }}/>
+      <Tab.Screen name="Search" component={TimKiem}  />
+      <Tab.Screen name="Notifications" component={ThongBao} options={{ headerShown: false }}/>
+      <Tab.Screen name="Messages" component={NhanTinScreen} options={{ headerShown: false }}/>
+    </Tab.Navigator>
+  );
+};
+
+
+
+// Tích hợp Stack Navigator và Bottom Tab Navigator
 const App = () => {
   return (
     <NavigationContainer>
-      
-      <Stack.Navigator initialRouteName="DangBai">
-
-
-        <Stack.Screen name="QuenMatKhau"
-          component={QuenMatKhauScreen} 
-          options={{ headerShown: false }}
-           />
+      <Stack.Navigator initialRouteName="DangKiTinhNguyenVienScreen">
         <Stack.Screen
-          name="XacMinhOTP"
-          component={XacMinhOtpScreen}
-          options={{ headerShown: false }} // You can set headerShown to true if you want to show the header
-        /> 
-         <Stack.Screen
-          name="DangKiDulich"
-          component={DangKiDulichScreen}
-          options={{ headerShown: false }} // You can set headerShown to true if you want to show the header
+          name="BottomTabs"
+          component={BottomTabs}
+          options={{ headerShown: false }} // Ẩn tiêu đề khi sử dụng Bottom Tabs
         />
-        
-         <Stack.Screen
-          name="DangBai"
-          component={DangBaiScreen}
-          options={{ headerShown: false }} // You can set headerShown to true if you want to show the header
-        />    
-         <Stack.Screen
-          name="NhanTin"
-          component={NhanTinScreen}
-          options={{ headerShown: false }} // You can set headerShown to true if you want to show the header
-        />  
-         <Stack.Screen 
-          name="DangKy" 
-          component={DangKy} 
-          options={{ headerShown: false }} // You can set headerShown to true if you want to show the header.
-        />
-        <Stack.Screen 
-          name="DangNhap" 
-          component={DangNhap} 
-          options={{ headerShown: false }} // You can set headerShown to true if you want to show the header
-        />
-         <Stack.Screen 
-          name="DoiMK" 
-          component={DoiMk} 
-          options={{ headerShown: false }} // You can set headerShown to true if you want to show the header
-        />
-         <Stack.Screen 
-          name="TimKiemBanDuLich" 
-          component={TimKiemBanDuLich} 
-          options={{ headerShown: false }} // You can set headerShown to true if you want to show the header
-        />
-        <Stack.Screen 
-          name="ThongTinCaNhan" 
-          component={ThongTinCaNhan} 
-          options={{ headerShown: false }} // You can set headerShown to true if you want to show the header
-        />
-        <Stack.Screen 
-          name="TimKiem" 
-          component={TimKiem} 
-          options={{ headerShown: false }} // You can set headerShown to true if you want to show the header
-        />
-        <Stack.Screen 
-          name="Blog" 
-          component={Blog} 
-          options={{ headerShown: false }} // You can set headerShown to true if you want to show the header
-        />
-         <Stack.Screen 
-          name="ThongBao" 
-          component={ThongBao} 
-          options={{ headerShown: false }} // You can set headerShown to true if you want to show the header
-        />
-
-
+        <Stack.Screen name="QuenMatKhau" component={QuenMatKhauScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="XacMinhOTP" component={XacMinhOtpScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="DangBai" component={DangBaiScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="DangKy" component={DangKy} options={{ headerShown: false }} />
+        <Stack.Screen name="DangNhap" component={DangNhap} options={{ headerShown: false }} />
+        <Stack.Screen name="DoiMK" component={DoiMk} options={{ headerShown: false }} />
+        <Stack.Screen name="TimKiemBanDuLich" component={TimKiemBanDuLich} options={{ headerShown: false }} />
+        <Stack.Screen name="ThongTinCaNhan" component={ThongTinCaNhan} options={{ headerShown: false }} />
+        <Stack.Screen name="Blog" component={Blog} options={{ headerShown: false }} />
+        <Stack.Screen name="DangKiTinhNguyenVienScreen" component={DangKiTinhNguyenVienScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
-
     </NavigationContainer>
   );
 };
---
+++ screens/DangKiTinhNguyenVienScreen.js
@@ -0,0 +1,168 @@
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text, TextInput, TouchableOpacity, StyleSheet, Image, KeyboardAvoidingView, Platform,ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const DangKiTinhNguyenVienScreen = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [address, setAddress] = useState('');
+    const [reason, setReason] = useState('');
+
+    const navigation = useNavigation();
+
+    const handleBack = () => {
+        navigation.goBack();
+    };
+
+    const handleSubmit = () => {
+        console.log({
+            name,
+            email,
+            phone,
+            address,
+            reason,
+        });
+    };
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.headerRow}>
+                        <TouchableOpacity style={styles.backButton} onPress={handleBack}>
+                            <Image
+                                source={{ uri: "https://cdn-icons-png.flaticon.com/128/130/130882.png" }}
+                                style={styles.backIcon}
+                            />
+                        </TouchableOpacity>
+                        <Text style={styles.title}> Đăng kí tình nguyện viên </Text>
+                    </View>
+
+            <KeyboardAvoidingView
+            
+                style={styles.container}
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0} // Adjust this value if needed
+            ><ScrollView >
+                <View style={styles.inner}>
+                
+                    
+                    <View style={styles.formContainer}>
+                    <Text style={styles.titlei}>Họ và tên</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Họ và tên"
+                            value={name}
+                            onChangeText={setName}
+                        />
+                        <Text style={styles.titlei}>Họ và tên</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Email"
+                            value={email}
+                            onChangeText={setEmail}
+                            keyboardType="email-address"
+                        />
+                        <Text style={styles.titlei}>Họ và tên</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Số điện thoại"
+                            value={phone}
+                            onChangeText={setPhone}
+                            keyboardType="phone-pad"
+                        />
+                        <Text style={styles.titlei}>Họ và tên</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Địa chỉ"
+                            value={address}
+                            onChangeText={setAddress}
+                        />
+                        <Text style={styles.titlei}>Họ và tên</Text>
+                        <TextInput
+                            style={[styles.input, styles.textArea]}
+                            placeholder="Lý do tham gia"
+                           
+                              
+                            multiline={true} // Cho phép nhập nhiều dòng
+                            numberOfLines={1} // Giới hạn số dòng hiển thị
+                            blurOnSubmit={true}
+                        />
+
+                        <TouchableOpacity style={styles.submitButton} onPress={handleSubmit}>
+                            <Text style={styles.submitText}>Gửi</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                </ScrollView>
+            </KeyboardAvoidingView>
+          
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+     
+    inner: {
+        padding: 16,
+        flex: 1,
+        // backgroundColor: 'red',
+    },
+    headerRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: '10%',
+    },
+    backButton: {
+       marginLeft: '5%',
+        // marginRight: 16,
+    },
+    backIcon: {
+        width: 24,
+        height: 24,
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginLeft: '10%',
+    },
+    formContainer: {
+        marginBottom:"10%",
+        flex: 1,
+        justifyContent: 'center',
+        marginTop: '10%',
+    },
+    input: {
+        height: 40,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 8,
+        backgroundColor:'#EBEDED',
+        marginBottom: '5%',
+        paddingLeft: 8,
+    },
+    textArea: {
+        height: 100,
+        textAlignVertical: 'top',
+        
+    },
+    submitButton: {
+        backgroundColor: '#17C6ED',
+        padding: 12,
+        borderRadius: 8,
+        alignItems: 'center',
+    },
+    submitText: {
+        color: '#fff',
+        fontSize: 16,
+    },
+    titlei: {
+        marginBottom: '2%',
+        marginLeft:'1%'
+    }
+});
+
+export default DangKiTinhNguyenVienScreen;
