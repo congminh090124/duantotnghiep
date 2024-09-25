9e7ec9cb0acebc204c7053a5c01bdc5f0ad34f13
vo ngoc chinh <vongocchinh001@gmail.com>
2024-09-25 11:08:43 +0700

    code moi nhat

--- screens/DangKiDuLichScreen.js
+++ screens/DangKiDuLichScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Alert, StyleSheet, Image ,KeyboardAvoidingView ,Platform} from 'react-native';
+import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Alert, StyleSheet, Image, KeyboardAvoidingView, Platform } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Swiper from 'react-native-swiper';
@@ -12,31 +12,14 @@ const Tab = createBottomTabNavigator(); // tạo tab navigator
 
 const DangKiDulichScreen = () => {
     const [find, setFind] = useState('');
-    const handleBack = () => {
-        navigation.goBack();
-    };
-
-
-
-
     return (
-        
+
         <SafeAreaView style={styles.container}>
 
-            
- 
-<View style={styles.inner}>
-                <View style={styles.headerRow}>
-                    <TouchableOpacity style={styles.backButton} onPress={handleBack}>
-                        <Image
-                            source={require('../assets/buttonback.png')} // Đường dẫn tới hình ảnh trong assets
-                            style={styles.backIcon}
-                        />
-                    </TouchableOpacity>
 
 
-                
-                </View>
+            <View style={styles.inner}>
+
 
 
                 {/* <Image
@@ -93,19 +76,21 @@ const DangKiDulichScreen = () => {
                     từ việc chọn địa điểm, đặt vé...
                 </Text>
 
-                <TouchableOpacity style={styles.dkn} onPress={handleBack}>
+                <TouchableOpacity style={styles.dkn} >
                     <Text style={{ color: "#fff" }}>Đăng kí ngay</Text>
                 </TouchableOpacity>
 
+                 
 
-                <TouchableOpacity style={styles.tht} onPress={handleBack}>
+
+                <TouchableOpacity style={styles.tht}>
                     <Text style={{ color: "#000" }}>Tìm hiểu </Text>
                 </TouchableOpacity>
 
 
 
             </View>
-            
+
 
         </SafeAreaView>
 
@@ -120,7 +105,7 @@ const DangKiDulichScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 16,
+        // padding: 16,
         backgroundColor: '#fff',
     },
 
@@ -137,7 +122,7 @@ const styles = StyleSheet.create({
         marginRight: '50%',
         width: '50%',
         marginLeft: '10%',
-        left:20
+        left: 20
     },
     backButton: {
         height: '40%',
@@ -157,7 +142,7 @@ const styles = StyleSheet.create({
         marginLeft: '15%',
     },
 
-    
+
     button: {
         backgroundColor: 'blue',
         padding: 10,
@@ -189,7 +174,7 @@ const styles = StyleSheet.create({
         color: '#5B6D72',
         marginTop: '5%',
     },
-    
+
     image2: {
         width: 25,
         height: 25,
@@ -204,7 +189,7 @@ const styles = StyleSheet.create({
 
         // marginTop: '-20%',
         color: '#fff',
-        top:-240,
+        top: -240,
         fontWeight: 'bold',
         fontSize: 20,
         marginLeft: '-30%',
@@ -222,13 +207,13 @@ const styles = StyleSheet.create({
     },
     td: {
         // marginTop: '5%'
-        top:-40,
+        top: -40,
         fontStyle: 'italic',
         // backgroundColor: 'gray',
         borderTopLeftRadius: 10, //
-        borderTopRightRadius:10,
-        padding:10,
-        height:'auto',
+        borderTopRightRadius: 10,
+        padding: 10,
+        height: 'auto',
     },
 
     dkn: {
@@ -247,7 +232,7 @@ const styles = StyleSheet.create({
     },
     wrapper: {
         height: '60%',  // Đảm bảo chiều cao cho Swiper
-        borderRadius:40
+        borderRadius: 40
     },
     slide: {
         justifyContent: 'center',
--- screens/TrangChuScreen.js
+++ screens/TrangChuScreen.js
@@ -1,47 +1,61 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-  
+import Icon from 'react-native-vector-icons/Ionicons'; // Thư viện icon
+import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Alert, StyleSheet, Image, KeyboardAvoidingView, Platform } from 'react-native';
 
 import DangKiDulichScreen from './DangKiDuLichScreen';
 import DangKiTinhNguyenVienScreen from './DangKiTinhNguyenVienScreen';
-
+import TrangTimBanDuLich from './TrangTimBanDuLich';
+import DangBaiScreen from './DangBaiScreen';
 
 const Tab = createMaterialTopTabNavigator();
 
 export default function TrangChuScreen() {
-    return (
-     
-        
-        <Tab.Navigator 
-        screenOptions={{
-          tabBarLabelStyle: { 
-            fontSize: 13, 
-            textTransform: 'none',  // Giữ nguyên chữ không viết hoa
-            fontWeight: 'bold',
-          },
-          tabBarStyle: { 
-            backgroundColor: '#fff', // Màu nền trong suốt
-            marginTop:'6%',
-            elevation: 0, // Xóa shadow cho tab bar
-          },
-          tabBarIndicatorStyle: { 
-            backgroundColor: 'black', // Màu của đường underline dưới tab đang chọn
-            height: 2, // Độ dày của đường underline
-            width: '20%', // Chiều rộng của đường gạch dưới
-            marginLeft: '2%', // Canh giữa so với tab
-          },
-          tabBarActiveTintColor: 'black', // Màu chữ khi tab được chọn
-          tabBarInactiveTintColor: 'gray', // Màu chữ khi tab không được chọn
-          tabBarPressColor: 'transparent', // Khi nhấn vào tab
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarLabelStyle: {
+          fontSize: 13,
+          fontWeight: 'bold',
+        },
+        tabBarStyle: {
+          backgroundColor: '#fff', // Màu nền trong suốt
+          marginTop: '10%',
+          elevation: 0, // Xóa shadow cho tab bar
+        },
+        tabBarIndicatorStyle: {
+          backgroundColor: 'black', // Màu của đường underline dưới tab đang chọn
+          height: 2, // Độ dày của đường underline
+          width: '22%', // Chiều rộng của đường gạch dưới
+          marginLeft: '6%', // Canh giữa so với tab
+          marginBottom: '15%', // Canh dưới nếu cần
+        },
+        tabBarActiveTintColor: 'black', // Màu chữ khi tab được chọn
+        tabBarInactiveTintColor: 'gray', // Màu chữ khi tab không được chọn
+        tabBarPressColor: 'transparent', // Khi nhấn vào tab
+      }}
+    >
+      <Tab.Screen
+        name="Trang chủ"
+        component={TrangTimBanDuLich}
+      />
+      <Tab.Screen
+        name="Blog"
+        component={DangBaiScreen}
+      />
+      <Tab.Screen
+        name="Messages"
+        component={DangKiDulichScreen}
+        options={{
+          headerShown: false,
+          tabBarLabel: '', // Xóa tên tab
+          tabBarIcon: ({ size }) => (
+            <Image source={require('../assets/messages.png')} style={{ width: 20, height: 20 ,bottom:-15}} />
+          )
         }}
-        style={{ backgroundColor:'#fff' }} // Canh trên nếu cần
-      >
-        <Tab.Screen name="Bạn bè" component={DangKiDulichScreen} />
-        <Tab.Screen name="Khám phá" component={DangKiDulichScreen} />
-        <Tab.Screen name="Đã follow" component={DangKiDulichScreen} />
-        <Tab.Screen name="Đề xuất" component={DangKiTinhNguyenVienScreen} />
-      </Tab.Navigator >
-      
-    );
-  }
+      />
+
+    </Tab.Navigator>
+  );
+}
